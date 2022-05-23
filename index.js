0a94f141c7a45b1c96a3426419715ac44488b187
Thomas Meylaers <thomas.meylaers@gmail.com>
2022-05-23 12:15:48 +0200

    fixed pw

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var transporter = nodemailer.createTransport({
     secure: true, // true for 465, false for other ports
     auth: {
         user: 'noreply@freshpepperdesign.com', // your domain email address
-        pass: 'piripiri101' // your password
+        pass: process.env.FRESH_PW // your password
     }
 });
 
@@ -30,12 +30,47 @@ var transporter_maison = nodemailer.createTransport({
     secure: true,
     auth: {
         user: 'noreply@maisonlaventure.be',
-        pass: 'NoreplyMaison568'
+        pass: process.env.MAISON_PW
     }
 })
 
 app.get('/mailserver/test', (req, res) => {
-    res.send("TEST");
+    // var mailOptionsMaison = {
+    //     from: 'noreply <noreply@freshpepperdesign.com>',
+    //     to: 'thomas.meylaers@gmail.com',
+    //     subject: "Nieuw bericht van maisonlaventure.be",
+    //     html: `test
+    //             `
+    // };
+    // // Send mail
+    // transporter.sendMail(mailOptionsMaison, function (err, data) {
+    //     if (err) return res.status(500).send(err)
+    //     res.send('het lukte')
+    // });
+
+    // Mailoptions
+    var mailOptionsMaisonNoreply = {
+        from: 'noreply <noreply@maisonlaventure.be>',
+        to: 'thomas.meylaers@gmail.com',
+        subject: "Bedankt voor uw reservatie! [NOREPLY]",
+        html: `
+            <img style="width:15rem;" src="https://maisonlaventure.be/img/logo_transparent.png">
+            <h1>Bedankt voor uw reservatie!</h1>   
+                    <h3>
+                    Beste </h3>
+                    <h3>
+                    Bedankt voor je contactaanvraag via onze website.  Je mag binnen de 24u een antwoord van ons verwachten.<br>
+                     Wij nemen  spoedig verder contact met u op.</h3>
+                     <h3>
+                    Voor meer info mail naar <a href="mailto:info@maisonlaventure.be">info@maisonlaventure.be</a> </h3>
+                    `
+    };
+    // Send mail
+    transporter_maison.sendMail(mailOptionsMaisonNoreply, function (err, data) {
+        if (err) return res.status(500).send(err)
+        res.send("het lukte")
+        // res.redirect(`https://maisonlaventure.be/${req.body.language}`)
+    });
 })
 
 app.post('/mailserver/maison', (req, res) => {
@@ -68,7 +103,7 @@ app.post('/mailserver/maison', (req, res) => {
                 <h3>
                 Beste ${req.body.voornaam}</h3>
                 <h3>
-                Via deze mail bevestigen we graag dat we uw aanvraag via het contactformulier op de website goed ontvangen hebben.<br>
+                Bedankt voor je contactaanvraag via onze website.  Je mag binnen de 24u een antwoord van ons verwachten.<br>
                  Wij nemen  spoedig verder contact met u op.</h3>
                  <h3>
                 Voor meer info mail naar <a href="mailto:info@maisonlaventure.be">info@maisonlaventure.be</a> </h3>
