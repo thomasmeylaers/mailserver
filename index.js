9636b9b5a09bc406358c247b3ce15900a2ebf766
Thomas Meylaers <thomas.meylaers@gmail.com>
2022-05-30 15:14:18 +0200

    captcha

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const nodemailer = require("nodemailer")
 const fs = require('fs')
 const mongoose = require('mongoose')
 const Reservering = require('./Reservering')
+const fetch = require("node-fetch")
 
 mongoose.connect(`mongodb+srv://Thomas:${process.env.MONGO_PW}@maison.z7upk72.mongodb.net/?retryWrites=true&w=majority`, () => {
     console.log("Connected to MongoDB")
@@ -118,43 +119,68 @@ app.get('/mailserver/test', (req, res) => {
 })
 
 app.post('/mailserver/simulatie', async (req, res) => {
-    if (req.body.voornaam == 'Henryrourl') {
-        res.send("Done")
-    } else {
-        // Sla reservering op in MongoDB
-        const new_reservering = await Reservering.create({
-            voornaam: req.body.voornaam,
-            achternaam: req.body.achternaam,
-            email: req.body.email,
-            telefoon: req.body.telefoon,
-            bericht: req.body.bericht
-        })
-        console.log(new_reservering)
 
-        // Mailoptions voor bericht naar management
-        var mailOptionsMaison = {
-            from: 'noreply <noreply@freshpepperdesign.com>',
-            to: 'thomas.meylaers@gmail.com',
-            subject: "Nieuw bericht van maisonlaventure.be",
-            html: `<h3>Voornaam: ${req.body.voornaam}</h3>
+    // CAPTCHA
+    // Check for captcha
+    if (
+        req.body.captcha === undefined ||
+        req.body.captcha === '' ||
+        req.body.captcha === null
+    ) {
+        return res.json({ "success": false, "msg": "Please select captcha" })
+    }
+
+    // Declare variables for request
+    const secretKey = process.env.CAPTCHA
+    const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}`
+
+    // Make request to google    
+    const response = await fetch(verifyUrl);
+    const body = await response.json();
+
+    console.log(body);
+
+    // Not succesful 
+    if (body.success !== undefined && !body.success) {
+        return res.json({ "success": false, "msg": "Failed Captcha Verification" })
+    }
+    console.log(req.body.voornaam)
+    // return res.json({ "success": true, "msg": "Success" })
+
+    // Sla reservering op in MongoDB
+    const new_reservering = await Reservering.create({
+        voornaam: req.body.voornaam,
+        achternaam: req.body.achternaam,
+        email: req.body.email,
+        telefoon: req.body.telefoon,
+        bericht: req.body.bericht
+    })
+    console.log(new_reservering)
+
+    // Mailoptions voor bericht naar management
+    var mailOptionsMaison = {
+        from: 'noreply <noreply@freshpepperdesign.com>',
+        to: 'thomas.meylaers@gmail.com',
+        subject: "Nieuw bericht van maisonlaventure.be",
+        html: `<h3>Voornaam: ${req.body.voornaam}</h3>
                 <h3>Achternaam: ${req.body.achternaam}</h3>
                 <h3>E-mail: ${req.body.email}</h3>
                 <h3>Telefoonnummer: ${req.body.telefoon}</h3>
                 <h3>Bericht: ${req.body.bericht}</h3>
                 `
-        };
-        // Send mail
-        transporter.sendMail(mailOptionsMaison, function (err, data) {
-            if (err) return res.status(500).send(err)
-            res.redirect(`https://maisonlaventure.be/${req.body.language}`)
-        });
+    };
+    // Send mail
+    transporter.sendMail(mailOptionsMaison, function (err, data) {
+        if (err) return res.status(500).send(err)
+        res.redirect(`https://maisonlaventure.be/${req.body.language}`)
+    });
 
-        // Mailoptions
-        var mailOptionsMaisonNoreply = {
-            from: 'noreply <noreply@maisonlaventure.be>',
-            to: 'thomas.meylaers@gmail.com',
-            subject: "Bedankt voor uw reservatie! [NOREPLY]",
-            html: `
+    // Mailoptions
+    var mailOptionsMaisonNoreply = {
+        from: 'noreply <noreply@maisonlaventure.be>',
+        to: 'thomas.meylaers@gmail.com',
+        subject: "Bedankt voor uw reservatie! [NOREPLY]",
+        html: `
         <img style="width:15rem;" src="https://maisonlaventure.be/img/logo_transparent.png">
         <h1>Bedankt voor uw reservatie!</h1>   
                 <h3>
@@ -165,13 +191,13 @@ app.post('/mailserver/simulatie', async (req, res) => {
                  <h3>
                 Voor meer info mail naar <a href="mailto:info@maisonlaventure.be">info@maisonlaventure.be</a> </h3>
                 `
-        };
-        // Send mail
-        transporter_maison.sendMail(mailOptionsMaisonNoreply, function (err, data) {
-            if (err) return res.status(500).send(err)
-            res.redirect(`https://maisonlaventure.be/${req.body.language}`)
-        });
-    }
+    };
+    // Send mail
+    transporter_maison.sendMail(mailOptionsMaisonNoreply, function (err, data) {
+        if (err) return res.status(500).send(err)
+        res.redirect(`https://maisonlaventure.be/${req.body.language}`)
+    });
+
 })
 
 app.post('/mailserver/maison', async (req, res) => {
